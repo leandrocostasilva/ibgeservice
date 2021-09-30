4746b86679f0669a84c1bf0655676e0463c3d735
Leandro <leandrocosta.silva@gmail.com>
2021-09-30 15:42:14 -0300

    Commit inicial

---
+++ src/main/java/br/com/leandro/kafkaline/ibjeservice/IbjeserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandro.kafkaline.ibjeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbjeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbjeserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandro/kafkaline/ibjeservice/IbjeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandro.kafkaline.ibjeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbjeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
